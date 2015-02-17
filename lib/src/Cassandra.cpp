52bf22dd47153cf314a7b8cf5ba2d358c246df44
Xavi Artigas <xavierartigas@yahoo.es>
2015-02-17 22:43:58 +0000

    Allow building the libs on non-windows machines.

--- lib/src/Cassandra.cpp
+++ lib/src/Cassandra.cpp
@@ -1,17 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
-#include <crtdbg.h>
 #include <stdio.h>
 #include <memory.h>
 #include "Cassandra.h"
 
+#ifdef _WIN32
+#include <crtdbg.h>
 #ifdef _DEBUG
 #ifndef DBG_NEW
 #define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
 #define new DBG_NEW
 #endif
 #endif  // _DEBUG
+#endif
 
 namespace Cass {
 
