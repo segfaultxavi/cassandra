3e61cbc110e47870849ed42c8a03da3b7f8d198e
Xavi Artigas <xavierartigas@yahoo.es>
2015-02-19 22:37:57 +0100

    Add time and memory counters to test1

--- test1/src/test1.cpp
+++ test1/src/test1.cpp
@@ -1,5 +1,20 @@
 #include "Game1.h"
 #include <stdio.h>
+#include <time.h>
+
+#ifdef _WIN32
+# include <Windows.h>
+# include <psapi.h>
+
+unsigned int used_memory () {
+	PROCESS_MEMORY_COUNTERS counter;
+	GetProcessMemoryInfo (GetCurrentProcess (), &counter, sizeof (counter));
+	return (unsigned int)counter.PeakWorkingSetSize;
+}
+
+#else
+# include <sys/sysinfo.h>
+#endif
 
 Game1::Renderer *Game1::g_renderer;
 
@@ -16,6 +31,7 @@ public:
 };
 
 int main (int argc, char *argv[]) {
+	clock_t time;
     Renderer renderer;
 	Game1::g_renderer = &renderer;
 	
@@ -23,14 +39,17 @@ int main (int argc, char *argv[]) {
 	printf ("\E[2J");
 	Cass::Solver *solver = current_state->get_solver ();
 	solver->add_start_point (current_state);
+	time = clock ();
 	int i = 0;
 	while (!solver->done ()) {
 		solver->process ();
 		i++;
 	}
+	time = clock () - time;
 	current_state->render (1.f);
 	printf ("\E[%d;%dHMap size is %dx%d\n", current_state->get_map_size_y () + 2, 1, current_state->get_map_size_x (), current_state->get_map_size_y ());
-	printf ("Processed %d nodes\n", i);
+	printf ("Processed %d nodes in %gms and used %gMB\n", i, 1000 * time / (float)CLOCKS_PER_SEC, used_memory () / (float)(1024 * 1024));
+
 	delete current_state;
 	delete solver;
 }
